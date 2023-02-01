b74e1092e4613e564c2794fb4caec842afd2dce0
Kiran <colkiran@gmail.com>
2023-02-01 09:06:50 +0530

    commit

---
+++ Day03/O01Numbers.py
@@ -0,0 +1,39 @@
+
+a = 10
+b = -10
+
+print(f"a :{a}")
+print(type(a))
+
+print(f"b :{b}")
+print(type(b))
+print("-" * 40)
+
+c = 10.5
+d = - 10.5
+
+print(f"c :{c}")
+print(type(c))
+
+print(f"d :{d}")
+print(type(d))
+print("-" * 40)
+
+e = +2e3
+f = -2e3
+
+print(f"e :{e}")
+print(type(e))
+
+print(f"f :{f}")
+print(type(f))
+print("-" * 40)
+
+g = 10 + 4j
+h = 10 - 4j
+
+print(f"g :{g}")
+print(type(g))
+
+print(f"h :{h}")
+print(type(h))
